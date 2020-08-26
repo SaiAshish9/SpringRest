a75fc394efadcaa5bd77c03814e2e34f317fa827
SaiAshish9 <saiashish3760@gmail.com>
2020-08-26 23:45:58 +0530

    autoconfiguration implemented

--- src/main/java/com/basics/restapi/RestapiApplication.java
+++ src/main/java/com/basics/restapi/RestapiApplication.java
@@ -2,13 +2,22 @@ package com.basics.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class RestapiApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hi");
-		SpringApplication.run(RestapiApplication.class, args);
+		ApplicationContext applicationContext = 
+				SpringApplication.run(RestapiApplication.class, args);
+          	
+	    for(String name:applicationContext.getBeanDefinitionNames()) {
+	      System.out.println(name);	
+	    }
 	}
-
 }
+
+//netstat -aon | find "8080"
+//taskkill /F /PID 2222
+
