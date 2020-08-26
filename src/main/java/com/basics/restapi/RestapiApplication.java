b73ebd28732e7f1e1c001106341e79e6c032386e
SaiAshish9 <saiashish3760@gmail.com>
2020-08-26 22:41:58 +0530

    initial commit

---
+++ src/main/java/com/basics/restapi/RestapiApplication.java
@@ -0,0 +1,14 @@
+package com.basics.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basics/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basics.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
